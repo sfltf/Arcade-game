2620f7f206c42392b83af847d5603855f4f744e1
Tengfei Lian <2837526923@qq.com>
2017-06-24 22:35:44 +0800

    重新格式代码格式

--- js/app.js
+++ js/app.js
@@ -12,23 +12,23 @@ var Enemy = function() {
 
 //初始化小虫子y位置
 var enemyLoc = function() {
-    for(var i = 0; i < allEnemies.length; i++) {
-        allEnemies[i].y = 58 + 83 * i;
+        for (var i = 0; i < allEnemies.length; i++) {
+            allEnemies[i].y = 58 + 83 * i;
+        }
     }
-}
-// 此为游戏必须的函数，用来更新敌人的位置
-// 参数: dt ，表示时间间隙
+    // 此为游戏必须的函数，用来更新敌人的位置
+    // 参数: dt ，表示时间间隙
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
     this.x += this.speed;
     //让小虫子不停地从出发点出发
-    if(this.x > 550) {
+    if (this.x > 550) {
         this.x = -10;
         enemyLoc();
     }
-     //如果玩家到达对岸，则小虫子停止移动
-    if(player.y < 0) {
+    //如果玩家到达对岸，则小虫子停止移动
+    if (player.y < 0) {
         this.speed = 0;
     }
 };
@@ -40,7 +40,7 @@ Enemy.prototype.render = function() {
 
 //实例化多个小虫子
 var allEnemies = [];
-for(var i = 0; i < 3; i ++) {
+for (var i = 0; i < 3; i++) {
     var enemy = new Enemy();
     allEnemies.push(enemy);
 }
@@ -57,16 +57,16 @@ var Player = function() {
 
 Player.prototype.update = function(dt) {
     //碰撞检测机制
-       for(var i = 0; i < allEnemies.length; i++){
-        if(Math.abs(this.x - allEnemies[i].x) < 55){
-            if((Math.abs(this.y - allEnemies[i].y)) < 40){
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (Math.abs(this.x - allEnemies[i].x) < 55) {
+            if ((Math.abs(this.y - allEnemies[i].y)) < 40) {
                 this.x = 300;
                 this.y = 400;
             }
-       }
+        }
     }
-     //让人物超过canvas的部分消失
-    if(this.y < 0) {
+    //让人物超过canvas的部分消失
+    if (this.y < 0) {
         ctx.globalCompositeOperation = 'source-atop';
     }
 };
@@ -78,52 +78,52 @@ Player.prototype.render = function() {
 
 //两个目的，1.让人物可以移动，2，让人物不能超出画面
 Player.prototype.handleInput = function(move) {
-       switch(move) {
+    switch (move) {
         case "left":
-        if(this.x < 30){
-            this.x = 3;
-        } else{
-           this.x -= 101; 
-        }
-        break;
+            if (this.x < 30) {
+                this.x = 3;
+            } else {
+                this.x -= 101;
+            }
+            break;
         case "right":
-         if(this.x > 400){
-            this.x = 401;
-        } else{
-           this.x += 101; 
-        };
-        break;
+            if (this.x > 400) {
+                this.x = 401;
+            } else {
+                this.x += 101;
+            };
+            break;
         case "up":
-         if(this.y < -10){
-            this.y = -15;
-        } else{
-           this.y -= 83; 
-        };
-        break;
+            if (this.y < -10) {
+                this.y = -15;
+            } else {
+                this.y -= 83;
+            };
+            break;
         case "down":
-         if(this.y > 399){
-            this.y = 400;
-        } else{
-           this.y += 83; 
-        }
-        break;
-       }
+            if (this.y > 399) {
+                this.y = 400;
+            } else {
+                this.y += 83;
+            }
+            break;
+    }
 };
 
 //当玩家到达对岸后，出现相应通关的画面
-    var wins = function() {
-        if(player.y < 0) {
-            ctx.globalCompositeOperation = 'source-over';
-            var image = [
-                'images/Star.png',
-                'images/Star.png',
-                'images/Star.png'
-            ];
-            ctx.font = "bold 40px Arial";
-            ctx.strokeText("恭喜你，通关！", 150, 280);
-            ctx.fillText("恭喜你，通关！", 150, 280);
-            for(var i = 0; i < image.length; i ++) {
-            ctx.drawImage(Resources.get(image[i]), 140 + i *80, 240);
+var wins = function() {
+    if (player.y < 0) {
+        ctx.globalCompositeOperation = 'source-over';
+        var image = [
+            'images/Star.png',
+            'images/Star.png',
+            'images/Star.png'
+        ];
+        ctx.font = "bold 40px Arial";
+        ctx.strokeText("恭喜你，通关！", 150, 280);
+        ctx.fillText("恭喜你，通关！", 150, 280);
+        for (var i = 0; i < image.length; i++) {
+            ctx.drawImage(Resources.get(image[i]), 140 + i * 80, 240);
         }
     }
 };
@@ -143,7 +143,7 @@ var Gem = function(x, y) {
 
 //创建3个实例宝石，并且随机化宝石位置（只能在有石头的地方）
 var gemOrange = [];
-for(var j = 0; j < 3; j++) {
+for (var j = 0; j < 3; j++) {
     var gem = new Gem(Math.floor(Math.random() * 5) * 101, Math.floor(Math.random() * 3) * 83 + 58);
     gemOrange.push(gem);
 }
@@ -155,13 +155,13 @@ Gem.prototype.render = function() {
 
 //碰撞检测，并且将重新随机放置碰撞后的宝石位置
 Gem.prototype.update = function(dt) {
-     for(var j = 0; j < gemOrange.length; j++){
-        if(Math.abs(player.x - gemOrange[j].x) < 55){
-            if((Math.abs(player.y - gemOrange[j].y)) < 40){
+    for (var j = 0; j < gemOrange.length; j++) {
+        if (Math.abs(player.x - gemOrange[j].x) < 55) {
+            if ((Math.abs(player.y - gemOrange[j].y)) < 40) {
                 gemOrange[j].x = Math.floor(Math.random() * 5) * 101;
                 gemOrange[j].y = Math.floor(Math.random() * 3) * 83 + 58;
             }
-       }
+        }
     }
 };
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
